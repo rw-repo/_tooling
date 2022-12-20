3d14a3b293fff048d8f66175e2e6e984ee4e28ac
RW <robertiweber3@gmail.com>
2022-12-19 20:27:29 -0700

    Update example2.js

--- sonarqube/vulnerable_code_examples/SQL Injection/example2.js
+++ sonarqube/vulnerable_code_examples/SQL Injection/example2.js
@@ -18,6 +18,19 @@ new mysql.Database(
 	password: 'password',
 	database: 'test'
 }
+	
+/*   Remediation
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.MYSQL_URL,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+connection.connect();
+*/
+	
 ).connect(function(error)
 {
 	var the_Query =
